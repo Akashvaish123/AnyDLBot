1342a4a229ffa45ba540be86dcfbb6c2945bdefa
Akashvaish123 <70126270+Akashvaish123@users.noreply.github.com>
2020-08-24 13:11:37 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,5 @@
 class Translation:
-    START_TEXT = """This is an @AnyDLBot clone created using the Open Source code available.
-Source Code: https://github.com/SpEcHiDe/AnyDLBot
+    START_TEXT = """Hi guys this is Anydlbot by AV team and join @AV_moviesHd and @AV_Bot_Developers to discuss about this bot
 © @AnyDLBot"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
